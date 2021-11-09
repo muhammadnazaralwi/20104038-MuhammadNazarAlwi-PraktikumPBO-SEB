8a8646c2d3c1d3fae1892dacc230df77840ac79d
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-11-09 12:57:26 +0700

    Exercise SOLVED - Create banking simulation system for learning about package, import, `this` keyword, etc

---
+++ PraktikumPemrogramanBerorientasiObjek/src/main/java/modul5/tugas/perbankan/Tabungan.java
@@ -0,0 +1,26 @@
+package modul5.tugas.perbankan;
+
+public class Tabungan {
+    private int saldo;
+
+    public Tabungan(int saldo) {
+        this.saldo = saldo;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public boolean ambilUang(int jumlah) {
+        if (saldo >= jumlah) {
+            saldo -= jumlah;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void simpanUang(int jumlah) {
+        saldo += jumlah;
+    }
+}
