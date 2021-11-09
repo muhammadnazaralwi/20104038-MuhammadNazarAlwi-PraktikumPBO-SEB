65d642e74108411c8699e83cda32deca53af6815
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-11-09 12:33:14 +0700

    Create `Mahasiswa` class for learning how `this` keyword can invoke the constructor

---
+++ PraktikumPemrogramanBerorientasiObjek/src/main/java/modul5/praktikum/Mahasiswa.java
@@ -0,0 +1,19 @@
+package modul5.praktikum;
+
+public class Mahasiswa {
+    private int nrp;
+    private String nama;
+
+    public Mahasiswa() {
+        this(0, "");
+    }
+
+    public Mahasiswa(String nama) {
+        this(0, nama);
+    }
+
+    public Mahasiswa(int nrp, String nama) {
+        this.nrp = nrp;
+        this.nama = nama;
+    }
+}
