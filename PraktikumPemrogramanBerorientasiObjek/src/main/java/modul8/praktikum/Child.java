d18a95f57473f2c853a0f82f3b882b794da01c21
Muhammad Nazar Alwi <20104038@ittelkom-pwt.ac.id>
2021-12-14 11:44:03 +0700

    Practicing polymorphism concept

---
+++ PraktikumPemrogramanBerorientasiObjek/src/main/java/modul8/praktikum/Child.java
@@ -0,0 +1,10 @@
+package modul8.praktikum;
+
+public class Child extends Parent {
+    int x = 10;
+
+    @Override
+    public void info() {
+        System.out.println("Ini kelas Child");
+    }
+}
---
+++ PraktikumPemrogramanBerorientasiObjek/src/main/java/modul8/praktikum/Parent.java
@@ -0,0 +1,9 @@
+package modul8.praktikum;
+
+public class Parent {
+    int x = 5;
+
+    public void info() {
+        System.out.println("Ini kelas Parent");
+    }
+}
